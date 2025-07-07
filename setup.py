ef37670dfb072e7d6df81408e19cc1fff562fa4e
Duy Luong <luongducduy.cantho@gmail.com>
2025-07-07 15:33:12 +0700

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="skedgo_mkdocs_theme",
-    version="0.3.3",
+    version="0.3.4",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
