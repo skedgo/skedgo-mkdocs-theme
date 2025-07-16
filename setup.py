f2e44c72463f000140d3ada94dcc863bc19a2e1b
Duy Luong <luongducduy.cantho@gmail.com>
2025-07-16 14:10:07 +0700

    Increase version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="skedgo_mkdocs_theme",
-    version="0.3.9",
+    version="0.4.0",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
